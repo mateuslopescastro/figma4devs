953c7a0c5bde2e348aa9e840eac2890b5728c09e
Mateus Lopes de Castro <mateuslopescastro@outlook.com>
2022-10-19 21:23:52 -0300

    initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 20 Oct 2022 00:21:15 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorZinc200": "#e4e4e7",
+  "ColorZinc500": "#71717a",
+  "ColorZinc800": "#27272a",
+  "ColorZinc900": "#18181b",
+  "ColorIndigo500": "#6366f1"
+};
